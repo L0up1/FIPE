c7aad7892f22c8d25edd851020a0794fb94fc52f
L0oup1 <thethiagoriguett@gmail.com>
2025-06-29 03:33:03 -0300

    projeto finalizado

--- demo/src/main/java/br/com/projeto/tabelafipe/demo/principal/Principal.java
+++ demo/src/main/java/br/com/projeto/tabelafipe/demo/principal/Principal.java
@@ -2,18 +2,21 @@ package br.com.projeto.tabelafipe.demo.principal;
 
 import br.com.projeto.tabelafipe.demo.model.Dados;
 import br.com.projeto.tabelafipe.demo.model.Modelos;
+import br.com.projeto.tabelafipe.demo.model.Veiculo;
 import br.com.projeto.tabelafipe.demo.service.ConsumoAPI;
 import br.com.projeto.tabelafipe.demo.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
-    private Scanner leitura = new Scanner(System.in);
-    private ConsumoAPI consumo = new ConsumoAPI();
-    private ConverteDados conversor = new ConverteDados();
+    private final Scanner leitura = new Scanner(System.in);
+    private final ConsumoAPI consumo = new ConsumoAPI();
+    private final ConverteDados conversor = new ConverteDados();
 
     private String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
     private String MARCAS = "/marcas";
@@ -39,12 +42,41 @@ public class Principal {
 
         System.out.println("Digite a marca desejada: ");
         String codigoMarca = leitura.nextLine().trim().toLowerCase();
-        String urlMarca = ENDERECO + tipoDeVeiculo + MARCAS + "/" + codigoMarca + "/modelos"; // Corrigido
+        String urlMarca = ENDERECO + tipoDeVeiculo + MARCAS + "/" + codigoMarca + "/modelos";
         String jsonModelos = consumo.obterDados(urlMarca);
         var modelosLista = conversor.obterDados(jsonModelos, Modelos.class);
         System.out.println("\nModelos dessa marca: ");
         modelosLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        System.out.println("\nDigite um trecho do nome do caro a ser buscado: ");
+        var nomeVeiculo = leitura.nextLine();
+
+        List<Dados> modelosFilter = modelosLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("\nModelos filtrados");
+        modelosFilter.forEach(System.out::println);
+
+        System.out.println("Digite o codigo do modelo para buscar valores: ");
+        var codigoModelo = leitura.nextLine();
+
+        String urlAnos = ENDERECO + tipoDeVeiculo + MARCAS + "/" + codigoMarca + "/modelos/" + codigoModelo + "/anos";
+        String jsonAnos = consumo.obterDados(urlAnos);
+        List<Dados> anos = conversor.obterLista(jsonAnos, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (Dados ano : anos) {
+            var enderecoAnos = ENDERECO + tipoDeVeiculo + MARCAS + "/" + codigoMarca + "/modelos/" + codigoModelo + "/anos/" + ano.codigo();
+            String jsonVeiculo = consumo.obterDados(enderecoAnos);
+            Veiculo veiculo = conversor.obterDados(jsonVeiculo, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os veiculos filtrados com avaliaçoes por ano:");
+        veiculos.forEach(System.out::println);
+
     }
 }
